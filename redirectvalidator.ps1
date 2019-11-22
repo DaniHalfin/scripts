66865e8d29e8f938d04be3cf5c3e9ece113057f4
Dani Halfin <daniha@microsoft.com>
2019-11-22 12:18:26 -0800

    first case handling done

--- redirectvalidator.ps1
+++ redirectvalidator.ps1
@@ -3,19 +3,43 @@
 #>
 
 ## REGEX patterns
-$sLinkInFilePattern     = "\[.*\]\(.*\)"
-$sURLPattern            = "http.*"
-$sInternalPathPattern   = "\/([A-Z]|[a-z])+\/.*"
-$sFileNamePattern       = "([A-Z]|[a-z])+\.([A-Z]|[a-z])+.*"
-$sAnchorPattern         = "(?<![\w\d])#([A-Z]|[a-z])+.*"
+$sLinkInFilePattern        = "(\[.*\]\(.*\))"
+$sValueInBracketsPattern   = "\(([^\[]*)\)"
+$sDocsURLPattern           = "https://docs\.microsoft\.com.*"
+$sURLPattern               = "http.*"
+$sInternalPathPattern      = "\/([A-Z]|[a-z])+\/.*"
+$sFileNamePattern          = "([A-Z]|[a-z])+\.([A-Z]|[a-z])+.*"
+$sAnchorPattern            = "(?<![\w\d])#([A-Z]|[a-z])+.*"
 
 ## Env vars
-$sDocsetPath            = 'C:\CPubGit\windows-docs-pr\windows\'
-$sBaseURL               = 'https://docs.microsoft.com/en-us/windows/'
+$sDocsetPath               = 'C:\CPubGit\windows-docs-pr\windows\'
+$sBaseURL                  = 'https://docs.microsoft.com/en-us/windows/'
+$sDocsPrefix               = 'https://docs.microsoft.com/'
+$sDocsPrefixWithLocale     = 'https://docs.microsoft.com/en-us/'
 
-
-$arrFileTree = Get-ChildItem -Path $sDocsetPath -Recurse -File -Exclude "*.json"
+$arrFileTree = Get-ChildItem -Path $sDocsetPath -Recurse -File -Include ('*.md','*.yml')
 foreach ($oFile in $arrFileTree)
 {
-   Select-String -Path $oFile.FullName -Pattern
+   foreach ($match in (Select-String -Path $oFile.FullName -Pattern $sLinkInFilePattern))
+   {
+      $sMatchLink = ($match.Matches.value -split $sValueInBracketsPattern)[1]
+      switch -Regex ($sMatchLink)
+      {
+         $sDocsURLPattern
+         {
+            $sAbsoluteURL  = [System.Net.HttpWebRequest]::Create($sMatchLink).GetResponse().ResponseUri.AbsoluteUri
+
+            $sAbsoluteURL  = $sAbsoluteURL -replace $sDocsPrefixWithLocale
+            $sAbsoluteURL  = $sAbsoluteURL -replace $sDocsPrefix
+
+            $sMatchLink    = $sMatchLink -replace $sDocsPrefixWithLocale
+            $sMatchLink    = $sMatchLink -replace $sDocsPrefix
+
+            if ($sMatchLink -ne $sAbsoluteURL)
+            {
+               $match.Filename
+            }
+         }
+      }
+   }
 }
