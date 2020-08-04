d976a99db75d316c56f1c42cc24ac411e15fc1db
Dani Halfin <daniha@microsoft.com>
2020-08-04 14:28:20 -0700

    adding new script to insert includes after h1

--- includeinserter.ps1
+++ includeinserter.ps1
@@ -1,7 +1,7 @@
 $sTargetLocation = "C:\repos\windows-docs-pr\windows\security\threat-protection"
 $sInclude = "[!include[Prerelease information](../../includes/prerelease.md)]"
 
-$lTargetFiles = Get-ChildItem -Path $sTargetLocation -File -Filter "*.md" -Recurse
+$lTargetFilesg = Get-ChildItem -Path $sTargetLocation -File -Filter "*.md" -Recurse
 
 # Traverse all files under folder
 foreach ($oFile in $lTargetFiles) {
@@ -14,11 +14,9 @@ foreach ($oFile in $lTargetFiles) {
             $sLineToUpdate = $sLine
             break
         }
-        
     }
     
     # Add include message
     $sUpdate = $sLineToUpdate + "`n`n" + $sInclude
-    (Get-Content $oFile.FullName) -replace $sLineToUpdate , $sUpdate | Set-Content $oFile.FullName
-    
+    (Get-Content $oFile.FullName) -replace $sLineToUpdate , $sUpdate | Set-Content $oFile.FullName 
 }
